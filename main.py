fb44215df2dd0ef5ece781a0fca326a340a67d9b
Matt <94198330+Matt-8A@users.noreply.github.com>
2022-07-06 00:50:31 +0000

    added new libraries to use in the future

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import discord
+import os
+import requests
+import json
+
+my_secret = os.environ['KEYTOKEN']
+
+unusableRequest = 'Sorry, no action has been implemented yet ¯\_(ツ)_/¯'
+
+disClient = discord.Client()
+
+@disClient.event
+async def on_ready():
+ print('Connection Successful {0.user}'.format(disClient))
+
+@disClient.event
+async def on_message(userMessage):
+  if userMessage.author == disClient.user:
+    return
+
+  if userMessage.content.startswith('#help'):
+    await userMessage.channel.send(unusableRequest)
+
+
+disClient.run(os.getenv('KEYTOKEN'))
+
